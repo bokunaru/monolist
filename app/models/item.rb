57582bd3c7203df80b571d4dba5c65a68290bbb7
Hiroki Haruna <hiroki.haruna@gmail.com>
2016-01-26 01:50:35 +0000

    ranking controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,5 @@ class Item < ActiveRecord::Base
   has_many :want_users, through: :wants, source: :user
   has_many :haves , foreign_key: "item_id" , dependent: :destroy
   has_many :have_users, through: :haves, source: :user
+
 end
